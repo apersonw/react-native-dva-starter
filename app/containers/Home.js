fa04c8273ae538fc7113a55013e7389af67a1b57
武刚 <wugang@billbear.cn>
2019-02-24 23:43:52 +0800

    1.优化自助餐

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,46 +1,54 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Image } from 'react-native'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { StyleSheet, View, Image, FlatList, Text } from "react-native";
+import { connect } from "react-redux";
 
-import { Button } from '../components'
+import { Button } from "../components";
 
-import { NavigationActions } from '../utils'
+import { NavigationActions } from "../utils";
 
 @connect()
 class Home extends Component {
   static navigationOptions = {
-    tabBarLabel: 'Home',
+    tabBarLabel: "好友",
     tabBarIcon: ({ focused, tintColor }) => (
       <Image
-        style={[styles.icon, { tintColor: focused ? tintColor : 'gray' }]}
-        source={require('../images/house.png')}
+        style={[styles.icon, { tintColor: focused ? tintColor : "gray" }]}
+        source={require("../images/house.png")}
       />
-    ),
-  }
+    )
+  };
 
   gotoDetail = () => {
-    this.props.dispatch(NavigationActions.navigate({ routeName: 'Detail' }))
-  }
+    this.props.dispatch(NavigationActions.navigate({ routeName: "Detail" }));
+  };
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button text="Goto Detail" onPress={this.gotoDetail} />
-      </View>
-    )
+      <View style={styles.container} >
+        <Button text="查看详情111" onPress={this.gotoDetail} />
+        <FlatList
+          onRefresh={() => {
+            console.log("我要开始刷新了哈");
+          }}
+          refreshing={false}
+          data={[{ key: "a" }, { key: "b" }, { key: "c" }, { key: "d" }, { key: "e" }, { key: "f" }, { key: "g" }, { key: "h" }, { key: "i" }]}
+          renderItem={({ item }) => <Text >{item.key}</Text >}
+        />
+      </View >
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   icon: {
     width: 32,
-    height: 32,
-  },
-})
+    height: 32
+  }
+});
 
-export default Home
+export default Home;
